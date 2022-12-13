6575c0e79127231ff481388b546fd86876836abd
Moazam Ali <mozalikorai@gmail.com>
2022-12-13 18:52:06 +0500

    Added all code in one fiile index.js and added that to html. changed the decimal to hexadecimal conversion code. changed classes and id's in html. changed the name of variables in js

--- BintoDec/index.js
+++ BintoDec/index.js
@@ -1,20 +1,67 @@
-let button = document.querySelector("#button");
+// Binary to Decimal
+
+let binToDecButton = document.querySelector("#binaryButton");
 
 const binToDec = () => {
-    let binaryValue = document.querySelector("#binary").value;
-    let result = document.querySelector(".result");
-    if (binaryValue.length == 0) {
-        alert("Field is empty!");
-        result.innerHTML = null;
-        return;
-    }
-    let pow = 1,
-        dec = 0;
-    for (let i = binaryValue.length - 1; i >= 0; i--) {
-        dec += binaryValue[i] * pow;
-        pow *= 2;
-    }
-    result.innerHTML = dec;
+  let binaryValue = document.querySelector("#binaryInput").value;
+  let result = document.querySelector(".decimalResult");
+  if (binaryValue.length == 0) {
+    alert("Field is empty!");
+    result.innerHTML = null;
+    return;
+  }
+  let pow = 1,
+    dec = 0;
+  for (let i = binaryValue.length - 1; i >= 0; i--) {
+    dec += binaryValue[i] * pow;
+    pow *= 2;
+  }
+  result.innerHTML = dec;
 };
 
-button.addEventListener("click", binToDec);
+binToDecButton.addEventListener("click", binToDec);
+
+// Decimal To Hexadecimal
+
+let decToHexaButton = document.querySelector("#decimalButton");
+
+const dectoHex = () => {
+  let decValueInString = document.querySelector("#decimalInput").value;
+  let decValue = parseInt(decValueInString);
+  let result = document.querySelector(".hexaResult");
+  if (decValueInString.length == 0) {
+    alert("Field is empty!");
+    result.innerHTML = null;
+    return;
+  }
+  const arr = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  let hex = '';
+  while (decValue > 0) {
+    let rem = decValue % 16;
+    hex += arr[rem];
+    decValue = Math.floor(decValue / 16);
+  }
+  hex = hex.split("").reverse().join("");
+  result.innerHTML = hex;
+
+  console.log(typeof hex);
+  console.log(hex);
+};
+decToHexaButton.addEventListener("click", dectoHex);
