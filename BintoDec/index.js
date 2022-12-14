56c0e6fbdbaf1a26f6e035fc611cedbe00c9eb90
Mustafa Malik <mustafashakeel111@gmail.com>
2022-12-14 18:37:16 +0500

    hexa done

--- BintoDec/index.js
+++ BintoDec/index.js
@@ -189,53 +189,52 @@ decToOctButton.addEventListener("click", DecToOct);
 let otbButton = document.querySelector("#otbButton");
 
 const octalToBin = () => {
-  let otbValueInString = document.querySelector("#otbInput").value;
-  let result = document.querySelector(".otbResult");
+    let otbValueInString = document.querySelector("#otbInput").value;
+    let result = document.querySelector(".otbResult");
 
-  if (otbValueInString.length == 0) {
-    alert("Field is empty!");
-    result.innerHTML = null;
-    return;
-  }
-  
-  let binary = "";
-  let i =0 ;
-
-  while (otbValueInString[i]) 
-  {
-    switch (otbValueInString[i]) {
-    case '0':
-        binary += "000";
-        break;
-    case '1':
-        binary += "001";
-        break;
-    case '2':
-        binary += "010";
-        break;
-    case '3':
-        binary += "011";
-        break;
-    case '4':
-        binary += "100";
-        break;
-    case '5':
-        binary += "101";
-        break;
-    case '6':
-        binary += "110";
-        break;
-    case '7':
-        binary += "111";
-        break;
-    default:
-        alert(`\nInvalid Octal Digit ${otbValueInString[i]}`);
-        break;
+    if (otbValueInString.length == 0) {
+        alert("Field is empty!");
+        result.innerHTML = null;
+        return;
     }
-    i++;
-}
 
-result.innerHTML = binary;
+    let binary = "";
+    let i = 0;
+
+    while (otbValueInString[i]) {
+        switch (otbValueInString[i]) {
+            case '0':
+                binary += "000";
+                break;
+            case '1':
+                binary += "001";
+                break;
+            case '2':
+                binary += "010";
+                break;
+            case '3':
+                binary += "011";
+                break;
+            case '4':
+                binary += "100";
+                break;
+            case '5':
+                binary += "101";
+                break;
+            case '6':
+                binary += "110";
+                break;
+            case '7':
+                binary += "111";
+                break;
+            default:
+                alert(`\nInvalid Octal Digit ${otbValueInString[i]}`);
+                break;
+        }
+        i++;
+    }
+
+    result.innerHTML = binary;
 
 };
 
@@ -247,28 +246,28 @@ otbButton.addEventListener("click", octalToBin);
 let otdButton = document.querySelector("#otdButton");
 
 const octalToDec = () => {
-  let otdValueInString = document.querySelector("#otdInput").value;
-  let otdValue = parseInt(otdValueInString);
-  let result = document.querySelector(".otdResult");
-  
-  if (otdValueInString.length == 0) {
-    alert("Field is empty!");
-    result.innerHTML = null;
-    return;
-  }
-  
-  let  dec = 0, rem = 0;
-  let base = 1;
+    let otdValueInString = document.querySelector("#otdInput").value;
+    let otdValue = parseInt(otdValueInString);
+    let result = document.querySelector(".otdResult");
 
-  while (otdValue != 0)
-  {
-      rem = otdValue % 10;
-      dec = dec + (rem * base);
-      otdValue = Math.floor(otdValue / 10);
-      base = base * 8;
-  }
+    if (otdValueInString.length == 0) {
+        alert("Field is empty!");
+        result.innerHTML = null;
+        return;
+    }
 
-  result.innerHTML = dec;
+    let dec = 0,
+        rem = 0;
+    let base = 1;
+
+    while (otdValue != 0) {
+        rem = otdValue % 10;
+        dec = dec + (rem * base);
+        otdValue = Math.floor(otdValue / 10);
+        base = base * 8;
+    }
+
+    result.innerHTML = dec;
 
 };
 
@@ -312,3 +311,82 @@ const hexToDec = () => {
 
 };
 hexToDecButton.addEventListener("click", hexToDec);
+
+//hexadecimal to octal
+let hexToOctButton = document.querySelector("#htoButton");
+
+const hexToOct = () => {
+    let htoInString = document.querySelector("#htoInput").value.toUpperCase();
+    console.log(htoInString);
+    let hexValue = parseInt(htoInString);
+    let result = document.querySelector(".htoResult");
+    if (htoInString.length == 0) {
+        alert("Field is empty!");
+        result.innerHTML = null;
+        return;
+    }
+    let length = htoInString.length;
+    let base = 1;
+    let dec = 0;
+    for (let i = length - 1; i >= 0; i--) {
+        if (htoInString[i] >= '0' && htoInString[i] <= '9') {
+            console.log(htoInString[i]);
+            dec += (htoInString[i].charCodeAt(0) - 48) * base;
+            base = base * 16;
+        } else if (htoInString[i] >= 'A' && htoInString[i] <= 'F') {
+            console.log(htoInString[i]);
+            dec += (htoInString[i].charCodeAt(0) - 55) * base;
+            base = base * 16;
+        }
+    }
+    const arr = ["0", "1", "2", "3", "4", "5", "6", "7"]
+    let oct = "";
+    while (dec > 0) {
+        let rem = dec % 8;
+        oct += arr[rem];
+        dec = Math.floor(dec / 8);
+    }
+    console.log(oct)
+    console.log(typeof oct)
+    oct = oct.split("").reverse().join("");
+    result.innerHTML = oct;
+};
+hexToOctButton.addEventListener("click", hexToOct);
+
+//hexadecimal to Binary
+let hexToBinButton = document.querySelector("#htbButton");
+
+const hexToBin = () => {
+    let htbInString = document.querySelector("#htbInput").value.toUpperCase();
+    console.log(htbInString);
+    let hexValue = parseInt(htbInString);
+    let result = document.querySelector(".htbResult");
+    if (htbInString.length == 0) {
+        alert("Field is empty!");
+        result.innerHTML = null;
+        return;
+    }
+    let length = htbInString.length;
+    let base = 1;
+    let dec = 0;
+    for (let i = length - 1; i >= 0; i--) {
+        if (htbInString[i] >= '0' && htbInString[i] <= '9') {
+            console.log(htbInString[i]);
+            dec += (htbInString[i].charCodeAt(0) - 48) * base;
+            base = base * 16;
+        } else if (htbInString[i] >= 'A' && htbInString[i] <= 'F') {
+            console.log(htbInString[i]);
+            dec += (htbInString[i].charCodeAt(0) - 55) * base;
+            base = base * 16;
+        }
+    }
+    let bin = "";
+    while (dec > 0) {
+        let rem = dec % 2;
+        bin += rem;
+        dec = Math.floor(dec / 2);
+    }
+    bin = bin.split("").reverse().join("");
+    result.innerHTML = bin;
+};
+hexToBinButton.addEventListener("click", hexToBin);
